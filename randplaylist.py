056fd9238a00a4e5bd02a1a0e8e9df60c530ed89
Adanufgail <adanufgail@gmail.com>
2020-07-26 17:40:17 +0000

    Now igoring songs with downvotes

--- randplaylist.py
+++ randplaylist.py
@@ -5,6 +5,9 @@ import string
 import json
 import requests
 
+
+#creds should have 3 things in order on a line by themselves:
+#username, password, site URL without trailing /
 f = open("creds", "r")
 u=f.readline().rstrip()
 password=f.readline().rstrip()
@@ -23,15 +26,13 @@ tohash=password+s
 
 t=hashlib.md5(tohash).hexdigest()
 
-#response = requests.get("http://api.open-notify.org/iss-now.json")
-#print(response.json()['iss_position'])
-
 BASE=site+"/rest/"
 v="1.16.1"
 c="randomplaylist"
 randplay=23
 
 
+print "Emptying Playlist"
 
 while requests.get(BASE+"getPlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&id="+str(randplay)).json()['subsonic-response']['playlist']['songCount'] > 1:
 	#print requests.get(BASE+"getPlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&id="+str(randplay)).json()['subsonic-response']['playlist']['songCount']
@@ -40,8 +41,12 @@ while requests.get(BASE+"getPlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s
 ADD=50
 FIRST=1
 
+print "Adding songs"
 for X in requests.get(BASE+"getRandomSongs"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&size="+str(ADD)+"&musicFolderId=2").json()['subsonic-response']['randomSongs']['song']:
-	print requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIdToAdd="+X['id']).json()
-	if FIRST == 1:
-		FIRST=0
-		print requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIndexToRemove=0").json()
+	song=requests.get(BASE+"getSong"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&id="+str(X['id'])+"").json()['subsonic-response']['song']
+        rating=song.get('averageRating',3.0)
+	if float(rating) > 2.0:
+		print requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIdToAdd="+X['id']).json()
+		if FIRST == 1:
+			FIRST=0
+			print requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIndexToRemove=0").json()
