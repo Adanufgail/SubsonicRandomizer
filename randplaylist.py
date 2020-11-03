7c41af2bfa3e77471a3129a88bac959cf4b5a321
Adanufgail <adanufgail@gmail.com>
2020-11-03 17:30:25 +0000

    Updated

--- randplaylist.py
+++ randplaylist.py
@@ -4,6 +4,7 @@ import random
 import string
 import json
 import requests
+import sys
 
 
 #creds should have 3 things in order on a line by themselves:
@@ -21,32 +22,40 @@ def get_random_alphanumeric_string(length):
 s=get_random_alphanumeric_string(12)
 
 tohash=password+s
-
-
-
 t=hashlib.md5(tohash).hexdigest()
 
 BASE=site+"/rest/"
-v="1.16.1"
+#v="1.16.1" SUBSONIC
+v="1.15.0" #AIRSONIC
 c="randomplaylist"
-randplay=23
+randplay=0
+musicfolder=0
+X=0
 
-
-print "Emptying Playlist"
-
+print("Emptying Playlist")
+totalz=requests.get(BASE+"getPlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&id="+str(randplay)).json()['subsonic-response']['playlist']['songCount']
 while requests.get(BASE+"getPlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&id="+str(randplay)).json()['subsonic-response']['playlist']['songCount'] > 1:
-	#print requests.get(BASE+"getPlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&id="+str(randplay)).json()['subsonic-response']['playlist']['songCount']
-	print requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIndexToRemove=1").json()
+	result=requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIndexToRemove=1").json()
+	totalz=totalz-1
+	sys.stdout.write('\r'+str(totalz)+" remain")
+	sys.stdout.flush()
 
-ADD=50
+ADD=200
 FIRST=1
+Z=0
 
-print "Adding songs"
-for X in requests.get(BASE+"getRandomSongs"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&size="+str(ADD)+"&musicFolderId=2").json()['subsonic-response']['randomSongs']['song']:
-	song=requests.get(BASE+"getSong"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&id="+str(X['id'])+"").json()['subsonic-response']['song']
-        rating=song.get('averageRating',3.0)
-	if float(rating) > 2.0:
-		print requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIdToAdd="+X['id']).json()
-		if FIRST == 1:
-			FIRST=0
-			print requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIndexToRemove=0").json()
+print("Adding songs")
+while Z < ADD:
+	sys.stdout.write('\r'+"Adding "+str(ADD-Z)+" more to reach "+str(ADD))
+	sys.stdout.flush()
+	for X in requests.get(BASE+"getRandomSongs"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&size="+str(1)+"&musicFolderId="+str(musicfolder)).json()['subsonic-response']['randomSongs']['song']:
+		song=requests.get(BASE+"getSong"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&id="+str(X['id'])+"").json()['subsonic-response']['song']
+        	rating=song.get('averageRating',3.0)
+		starred=song.get('starred',0)
+		if float(rating) > 2.0:
+			if str(starred) == '0':
+				result=requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIdToAdd="+X['id']).json()
+				Z+=1
+				if FIRST == 1:
+					FIRST=0
+					result=requests.get(BASE+"updatePlaylist"+"?u="+u+"&f=json&v="+v+"&c="+c+"&t="+t+"&s="+s+"&playlistId="+str(randplay)+"&songIndexToRemove=0").json()
